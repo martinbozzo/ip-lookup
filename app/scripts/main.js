0239941887446143a7238968981062a515c60f02
Martín Javier Bozzo <mjbozzo@gmail.com>
2017-01-29 12:30:24 -0300

    Added first IP Lookup functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+$(function () {
+    $('#form').submit(function (event) {
+        event.preventDefault();
+
+        console.log("geting Location from IP ", $('form input').val());
+        $.getJSON('http://ipinfo.io/' + $('form input').val() + '/geo', function(data) {
+            console.log(data);
+            $('.data').html(JSON.stringify(data));
+        })
+    });
+});
